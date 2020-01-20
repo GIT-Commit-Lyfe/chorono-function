a314586e6cc7b4ea9a195f50b8f96c6f2a048dc8
Muhammad Ramadhoni <ramadhoni@keyreply.com>
2020-01-20 17:23:23 +0700

    breguet done

--- server/app.js
+++ server/app.js
@@ -7,12 +7,12 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-let index = 610
+let index = 651
 app.post("/write", function(req, res){
 
  
   //fs.writeFileSync(Date.now().toString() + ".txt", req.body.array.join("\r\n"))
-  fs.writeFile(index+ "-" +`Tudor `+req.body.title + ".txt", req.body.array.join("\r\n"), function(err){
+  fs.writeFile(index+ "-" +`Breguet `+req.body.title + ".txt", req.body.array.join("\r\n"), function(err){
     if(err){
       console.log(err)
       res.status(500).json({
